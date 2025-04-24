542afe1301efb0ebe61d172bbef78cd4c8c76121
Hoang Dao <dao.hoang@taptap.com.vn>
2025-04-24 11:10:20 +0700

    update

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Manager } from "./Manager";
-import { LoadingScreen } from "./screens/LoadingScreen";
-
-(async () => {
-    // init config and pixi app
-    await Manager.initialize(0xffffff);
-    // open loading screen for load assets
-    Manager.changeScreen(new LoadingScreen({isReconnect: false}));
-})();
